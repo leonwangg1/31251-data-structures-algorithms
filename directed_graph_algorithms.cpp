72e3fb41ae6205ee7493c720b6b51a282c32c363
leonwangg1 <leonwang148@gmail.com>
2020-06-10 19:07:38 +1000

    tidied up code, need fix shortest path algo returning something it cant reach

--- directed_graph_algorithms.cpp
+++ directed_graph_algorithms.cpp
@@ -23,9 +23,16 @@
 
 using namespace std;
 
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Computes the shortest distance from u to v in graph g.
+ * The shortest path corresponds to a sequence of vertices starting from u and ends at v,
+ * which has the smallest total weight of edges among all possible paths from u to v.
+ */
+
 template<typename T>
 int min_distance(directed_graph<T>& g, const std::map<T, T>& dijkstras, const std::unordered_set<T>& spt_set) {
-	int min_vertex;
+	
+  int min_vertex;
 	// Set to max int value
 	int min = std::numeric_limits<int>::max();
 	// For each vertex
@@ -39,12 +46,6 @@ int min_distance(directed_graph<T>& g, const std::map<T, T>& dijkstras, const st
 	}
 	return min_vertex;
 }
-
-/*
- * Computes the shortest distance from u to v in graph g.
- * The shortest path corresponds to a sequence of vertices starting from u and ends at v,
- * which has the smallest total weight of edges among all possible paths from u to v.
- */
 
 template <typename T>
 vector<vertex<T>> shortest_path(directed_graph<T> g, int u_id, int v_id) {
@@ -121,7 +122,7 @@ vector<vertex<T>> shortest_path(directed_graph<T> g, int u_id, int v_id) {
   return path;
 }
 
-/*
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  * Computes the strongly connected components of the graph.
  * A strongly connected component is a subset of the vertices
  * such that for every pair u, v of vertices in the subset,
@@ -207,7 +208,7 @@ vector<vector<vertex<T>>> strongly_connected_components(directed_graph<T> g){
 
 }
 
-/*
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  * Computes a topological ordering of the vertices.
  * For every vertex u in the order, and any of its
  * neighbours v, v appears later in the order than u.
@@ -254,13 +255,14 @@ vector<vertex<T>> topological_sort(directed_graph<T> g) { //Kahn's Algorithm or 
 
 }
 
-/*
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  * Computes the lowest cost-per-person for delivery over the graph.
  * u is the source vertex, which send deliveries to all other vertices.
  * vertices denote cities; vertex weights denote cities' population;
  * edge weights denote the fixed delivery cost between cities, which is irrelevant to 
  * the amount of goods being delivered. 
  */
+
 template <typename T>
 T low_cost_delivery(directed_graph<T> g, int u_id) {
 
@@ -274,13 +276,13 @@ T low_cost_delivery(directed_graph<T> g, int u_id) {
 
   for(auto a : g.get_vertices()){
     if(a.id !=u_id){
-     result.push_back(shortest_path(g, u_id, a.id)); //return vector of shortest path from root to other nodes
-      //if vertices return greater than 2, only return the last 2 
+     result.push_back(shortest_path(g, u_id, a.id)); //return vector of shortest path from root to other node
+    //check condition vv
     }
   }
 
   for(auto vector: result){
-    while(vector.size() > 2){ //if contains more than 2 vertices
+    while(vector.size() > 2){   //if vertices return greater than 2, only return the last 2 
       vector.erase(vector.begin());
     }
     for(auto v : vector){
@@ -304,7 +306,7 @@ T low_cost_delivery(directed_graph<T> g, int u_id) {
     for(int n : population){
       sumPopulation += n;
     }
-
+  
   return sumCost/sumPopulation;
 
 }
